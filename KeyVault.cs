b9124d996ae016b1675dea861fb026fb5bc83e8f
RFN2018 <44893745+RFN2018@users.noreply.github.com>
2019-01-25 15:26:05 +0000

    Add files via upload

---
+++ KeyVault.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace ConsoleApp1
+{
+    class KeyVault
+    {
+        public static async Task<string> GetKey(string secretId)
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                var secret = await keyVaultClient.GetSecretAsync(secretId)
+                    .ConfigureAwait(false);
+
+            return secret.Value;
+
+        }
+    }
+}
